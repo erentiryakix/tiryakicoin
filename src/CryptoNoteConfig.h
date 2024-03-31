b12b5e7b98513545cee6ae91a007a8afcf7782b6
erentiryakix <165542179+erentiryakix@users.noreply.github.com>
2024-03-31 08:54:24 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -22,8 +22,8 @@ const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 
 //TODO Specify total number of available coins
-//TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
-//TODO or you can define number explicitly UINT64_C(858986905600000000)
+//TODO ((uint64_t)(-1)) equals to 2 coins
+//TODO or you can define number explicitly UINT64_C(2)
 const uint64_t MONEY_SUPPLY                                  = ;
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
